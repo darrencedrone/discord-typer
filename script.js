d80c8f90d607c750a0cd76bcdde4a85049e3a0c0
Darren Cedrone <dcedrone@Darrens-MacBook-Air.local>
2020-09-15 19:38:59 -0400

    Added usable front end and copy button

--- script.js
+++ script.js
@@ -2,11 +2,8 @@ const getResult = () => {
   const word = document.getElementById('word').value
 
   const arr1 = word.split('')
-  console.log(arr1)
-  const arr2 = [""].concat(arr1)
-  console.log(arr2)
+  const arr2 = [''].concat(arr1)
   const newSentence = arr2.join(':regional_indicator_')
-  console.log(newSentence)
 
   const insertFormatting = (str, n) => {
     let ret = []
@@ -22,7 +19,33 @@ const getResult = () => {
 
   const joinedSentence = insertFormatting(newSentence, 21).join(': ')
   const regex = /:regional_indicator_ :/gi
-  const finalSentence = joinedSentence.replace(regex, "&nbsp&nbsp&nbsp")
-  document.getElementById('display').innerHTML = finalSentence + ":"
+  const finalSentence = joinedSentence.replace(regex, "   ")
+  const displaySentence = joinedSentence.replace(regex, "&nbsp&nbsp&nbsp")
+  document.getElementById('display').innerHTML = displaySentence + ":"
+}
 
+const copyText = () => {
+  const word = document.getElementById('word').value
+
+  const arr1 = word.split('')
+  const arr2 = [''].concat(arr1)
+  const newSentence = arr2.join(':regional_indicator_')
+
+  const insertFormatting = (str, n) => {
+    let ret = []
+    let i
+    let len
+
+    for (i = 0, len = str.length; i < len; i += n) {
+      ret.push(str.substr(i, n))
+    }
+
+    return ret
+  }
+
+  const joinedSentence = insertFormatting(newSentence, 21).join(': ')
+  const regex = /:regional_indicator_ :/gi
+  const finalSentence = joinedSentence.replace(regex, "   ")
+  const copyText = finalSentence + ":"
+  navigator.clipboard.writeText(copyText)
 }
